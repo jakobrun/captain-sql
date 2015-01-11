491d76719b710ae1a11bf664f0f22d5a933bcb84
jakobrun <jakob.runarsson@gmail.com>
2015-01-11 23:29:41 +0000

    add gulp test tasks

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,23 @@
 var gulp = require('gulp'),
   less = require('gulp-less'),
-  jshint = require('gulp-jshint');
+  jshint = require('gulp-jshint'),
+  mocha = require('gulp-mocha'),
+  exec = require('child_process').exec,
+  testSrc = './test/**/*.js';
 
 gulp.task('lint', function() {
   return gulp.src('./js/*.js')
     .pipe(jshint())
     .pipe(jshint.reporter('default'));
+});
+
+gulp.task('node-test', function () {
+  return gulp.src(testSrc, {read: false})
+    .pipe(mocha());
+});
+
+gulp.task('nw-test', function () {
+  exec('nw ./nw-test');
 });
 
 gulp.task('less', function() {
@@ -14,6 +26,7 @@ gulp.task('less', function() {
     .pipe(gulp.dest('./css'));
 });
 
-gulp.task('dev', function() {
+gulp.task('dev', ['less', 'node-test'], function() {
   gulp.watch(['./less/**/*.less'], ['less']);
+  gulp.watch(['./js/*.js', testSrc], ['node-test']);
 });
