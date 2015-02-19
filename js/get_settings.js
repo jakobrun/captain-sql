5bf8ea96085ee1d69e19dd1d213915159d42e03a
jakobrun <jakob.runarsson@gmail.com>
2015-02-19 21:52:38 +0000

    fix default settings

--- js/get_settings.js
+++ js/get_settings.js
@@ -8,7 +8,7 @@ var getDefaultSettings = function(baseDir) {
     "    name: 'My connection',\n" +
     "    host: 'myhost',\n" +
     "    user: '',\n" +
-    "    properties: {}\n" +
+    "    properties: {},\n" +
     "    schema: [{\n" +
     "      name: 'lib1',\n" +
     "      file: '" + baseDir + "/.gandalf/schema.json'\n" +
