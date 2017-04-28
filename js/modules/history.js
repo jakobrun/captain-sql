89f7e90eff425d2e0126744f9373c95b259e5a94
jakob <jakob.runarsson@gmail.com>
2017-04-28 15:03:08 +0000

    fix history

--- js/modules/history.js
+++ js/modules/history.js
@@ -1,32 +1,29 @@
 'use strict';
 import q from 'q';
-import {writeFile, appendFile, readFile}
-from 'fs';
+import { writeFile, readFile }
+  from 'fs';
 
 export default function getHistoryModel(options) {
   let buffer = [];
   let promise = q();
   const fileName = process.env.HOME + '/.gandalf/' + options.file;
   const history = {
-    push: function(item) {
-      promise = promise.then(function() {
+    push: function (item) {
+      promise = promise.then(function () {
         buffer.splice(0, 0, item);
         if (buffer.length > options.max) {
           buffer = buffer.slice(0, options.min);
-          return q.nfcall(writeFile, fileName, buffer.map(JSON.stringify).join(','));
-        } else {
-          return q.nfcall(appendFile, fileName, (buffer.length === 1 ? '' : ',') +
-            JSON.stringify(item));
         }
+        return q.nfcall(writeFile, fileName, buffer.map(JSON.stringify).join(','));
       });
       return promise;
     },
     list: () => buffer
   };
-  return q.nfcall(readFile, fileName).then(function(content) {
+  return q.nfcall(readFile, fileName).then(function (content) {
     buffer = JSON.parse('[' + content + ']');
     return history;
-  }, function() {
+  }, function () {
     return history;
   });
 }
