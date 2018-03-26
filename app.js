909d733df3a2200cde6f90c891fd46d0535de7ae
jakob <jakob.runarsson@gmail.com>
2018-03-26 19:24:27 +0000

    logo icons

--- app.js
+++ app.js
@@ -12,7 +12,11 @@ global.sharedObject = {
 }
 function createWindow() {
     // Create the browser window.
-    const win = new BrowserWindow({ width: 800, height: 600 })
+    const win = new BrowserWindow({
+        width: 800,
+        height: 600,
+        icon: path.join(__dirname, 'icons/png/64x64.png'),
+    })
 
     // and load the index.html of the app.
     win.loadURL(
