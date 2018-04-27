3772fccfc1e49ba1360524d2423e307f6c66a5c7
jakob <jakob.runarsson@gmail.com>
2018-04-27 11:38:55 +0000

    remember splitter position and use it on startup

--- src/views/splitter.ts
+++ src/views/splitter.ts
@@ -1,6 +1,14 @@
 export const createSplitter = m => {
+    const lastSplitterPos = localStorage.getItem('main.splitter.pos')
+    if (lastSplitterPos) {
+        document.body.style.setProperty(
+            '--editor-height',
+            lastSplitterPos + 'px'
+        )
+    }
     const onMove = e => {
         const y = Math.min(Math.max(30, e.pageY - 2), window.innerHeight - 30)
+        localStorage.setItem('main.splitter.pos', String(y))
         document.body.style.setProperty('--editor-height', y + 'px')
     }
     const onUp = () => {
